960e9bef988884fb7f216625d2e3ff6aee9315aa
Kristian Dupont <kristian@kristiandupont.com>
2020-02-13 16:10:48 +0100

    Change input enum, introduce detectCasing

--- README.md
+++ README.md
@@ -11,10 +11,10 @@ npm i @kristiandupont/recase
 ```javascript
 const recase = require('@kristiandupont/recase');
 
-const initialCase = 'snakeCase';
+const initialCase = 'snake';
 
-const toCamel = recase(initialCase, 'camelCase');
-const toDash = recase(initialCase, 'dashCase');
+const toCamel = recase(initialCase, 'camel');
+const toDash = recase(initialCase, 'dash');
 
 const s = 'some_string_Initially_set_with_snake_case';
 
@@ -25,19 +25,26 @@ console.log('Dash cased version:', toDash(s));
 // prints: "Dash cased version: some-string-initially-set-with-snake-case"
 ```
 
-## Reference
-This module exposes one function:
-```
+## recase
+```javascript
 recase(from, to)
 ```
 
-It takes two casing parameters which can both be one of `dashCase`, `snakeCase`, `camelCase` or `pascalCase`.
+Takes two casing parameters which can both be one of `dash`, `snake`, `camel` or `pascal`.
 It returns a function that converts strings from the `from` casing to the `to` casing.
+
+
+## detectCasing
+```javascript
+detectCasing(str)
+```
+
+Takes a string that has been formatted. It will attempt to detect the casing and return it according to the 
 
 
 ## Casings
 
- - `dashCase`: `example-string`
- - `snakeCase`: `example_string`
- - `camelCase`: `exampleString`
- - `pascalCase`: `ExampleString`
+ - `dash`: `example-string`
+ - `snake`: `example_string`
+ - `camel`: `exampleString`
+ - `pascal`: `ExampleString`
--- index.js
+++ index.js
@@ -1,34 +1,72 @@
-const R = require('ramda');
+import { head, tail, pipe, curry } from 'ramda';
+
+/**
+ * @typedef {'dash'|'snake'|'camel'|'pascal'} Casing
+ * @typedef {Casing|'mixed'} WideCasing
+ */
 
 const lowerCase = s => s.toLowerCase();
 const capitalize = s => s.charAt(0).toUpperCase() + s.slice(1);
+const trimSeparators = s => s.replace(/^(\-|_)+|(\-|_)+$/g, '');
 
 const parsers = {
-  dashCase: s => s.split('-'),
-  snakeCase: s => s.split('_'),
-  camelCase: s => s.split(/(?=[A-Z])/),
-  pascalCase: s => s.split(/(?=[A-Z])/),
+  dash: s => s.split('-'),
+  snake: s => s.split('_'),
+  camel: s => s.split(/(?=[A-Z])/),
+  pascal: s => s.split(/(?=[A-Z])/),
+  mixed: s =>
+    s
+      .split(/(?=[A-Z\-_])/)
+      .map(trimSeparators)
+      .filter(Boolean),
 };
 
 const composers = {
-  dashCase: a => a.map(lowerCase).join('-'),
-  snakeCase: a => a.map(lowerCase).join('_'),
-  camelCase: a =>
-    [a[0].toLowerCase(), ...R.map(capitalize, R.tail(a))].join(''),
-  pascalCase: a => R.map(capitalize, a).join(''),
+  dash: a => a.map(lowerCase).join('-'),
+  snake: a => a.map(lowerCase).join('_'),
+  camel: a => [head(a).toLowerCase(), ...tail(a).map(capitalize)].join(''),
+  pascal: a => a.map(capitalize).join(''),
 };
 
 /**
- * @param {'dashCase' | 'snakeCase' | 'camelCase' | 'pascalCase'} from
- * @param {'dashCase' | 'snakeCase' | 'camelCase' | 'pascalCase'} to
- * @return {(s: string) => string}
+ * @param {WideCasing} [from] source casing. If falsy or "mixed", it will split by all word boundaries
+ * @param {Casing} to target casing.
+ * @param {string} str string to recase
  */
-const recase = (from, to) =>
-  from === to
-    ? R.identity
-    : R.pipe(
-        parsers[from],
-        composers[to]
-      );
+const recase = curry((from, to, str) =>
+  from === to ? str : pipe(parsers[from || 'mixed'], composers[to])(str)
+);
 
-module.exports = recase;
+/**
+ * @param {string} str
+ * @returns {WideCasing | null} returns detected casing or null if unknown
+ */
+const detectCasing = str => {
+  /** @type {WideCasing | null} */
+  let result = null;
+
+  if (str.indexOf('_') !== -1) result = 'snake';
+
+  if (str.indexOf('-') !== -1) {
+    if (result) {
+      return 'mixed';
+    }
+    result = 'dash';
+  }
+
+  if (str !== str.toLowerCase()) {
+    if (result) {
+      return 'mixed';
+    }
+    // Name has uppercase letters. Check if first letter is uppercase.
+    const firstLetter = str.charAt(0);
+    result = firstLetter === firstLetter.toLowerCase() ? 'camel' : 'pascal';
+  }
+
+  return result;
+};
+
+module.exports = {
+  recase,
+  detectCasing,
+};
