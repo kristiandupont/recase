01a158276de3e86fe58acdd9731b0b608c41513d
Kristian Dupont <kristian@kristiandupont.com>
2024-06-13 18:16:26 +0000

    Update types

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,18 @@
-export declare type Casing = "dash" | "snake" | "camel" | "pascal";
+export declare type Casing =
+  | "dash"
+  | "screamingDash"
+  | "snake"
+  | "screamingSnake"
+  | "camel"
+  | "pascal"
+  | "prose";
 export declare type WideCasing = Casing | "mixed" | null;
 
 export function recase(from: WideCasing, to: Casing, str: string): string;
 export function recase(from: WideCasing, to: Casing): (str: string) => string;
 export function recase(from: WideCasing): (to: Casing, str: string) => string;
 export function recase(
-  from: WideCasing,
+  from: WideCasing
 ): (to: Casing) => (str: string) => string;
 
 export function detectCasing(str: string): WideCasing;
